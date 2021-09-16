505cc6cabbbcbb63aa22850ccb8141cf74389767
Victor Lira <victorliradev@gmail.com>
2021-09-15 21:15:23 -0300

    Update MenuMobile.js

--- src/components/Header/MenuMobile.js
+++ src/components/Header/MenuMobile.js
@@ -24,7 +24,7 @@ const MenuMobile = (props) => {
             <ul>
                 <li>Users</li>
                 <li>Companies</li>
-                <li>Founders</li>
+                <li>Found</li>
                 <li>
                     <div>
                         <AiFillFacebook style={{ fontSize: "25px" }} />
